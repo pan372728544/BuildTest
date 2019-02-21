aea965c296d544668f579e0afc42d79aca2c4f2a
panzhijun <panzhijun@gomeplus.com>
2019-02-21 11:43:10 +0800

    xcodebuild test

---
+++ BuildTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuildTest
+//
+//  Created by panzhijun on 2019/2/21.
+//  Copyright © 2019 panzhijun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BuildTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BuildTest
+//
+//  Created by panzhijun on 2019/2/21.
+//  Copyright © 2019 panzhijun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
